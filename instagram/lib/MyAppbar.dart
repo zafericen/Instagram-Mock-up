bf107f30130a9fad0cf32180b0072b091b1d2cef
zafericen <zafericen1907@gmail.com>
2023-03-06 22:58:28 +0300

    AppBar Custumized

---
+++ instagram/lib/MyAppbar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CostumizedAppBar extends StatefulWidget with PreferredSizeWidget {
+  const CostumizedAppBar({super.key});
+
+  @override
+  State<CostumizedAppBar> createState() => _CostumizedAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CostumizedAppBarState extends State<CostumizedAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: PopupMenuButton(
+        child: const Text(
+          "Instagram ",
+          style: TextStyle(fontStyle: FontStyle.italic),
+        ),
+        itemBuilder: (context) => [
+          PopupMenuItem(
+            value: 1,
+            child: Text("Takip"),
+          ),
+          PopupMenuItem(
+            value: 2,
+            child: Text("Favoriler"),
+          )
+        ],
+        onSelected: (value) {
+          if (value == 1) {
+            print("takip pressed");
+          } else if (value == 2) {
+            print("Favoriler pressed");
+          }
+        },
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {
+              print("kalp pressed");
+            },
+            icon: Icon(Icons.heart_broken_rounded)),
+        IconButton(
+            onPressed: () {
+              print("mesaj pressed");
+            },
+            icon: Icon(Icons.message))
+      ],
+    );
+  }
+}
